891746335ed226be423b56f8261fa07f925dbeee
Nicasiomarques <nicasiomarques18@gmail.com>
2024-01-04 14:21:18 +0100

    feat: implement register

---
+++ components/auth/register-form/fields-config.ts
@@ -0,0 +1,20 @@
+export const fieldConfigs = [
+  {
+    name: "name" as const,
+    label: "Name",
+    type: "text",
+    placeholder: "Mingo Bilie",
+  },
+  {
+    name: "email" as const,
+    label: "Email",
+    type: "email",
+    placeholder: "mingo.bilie@example.com",
+  },
+  {
+    name: "password" as const,
+    label: "Password",
+    type: "password",
+    placeholder: "******",
+  },
+];
